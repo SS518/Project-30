c7d1deee2afe76b06a47a20ef0a50a58eb7263e8
SS518 <72257999+SS518@users.noreply.github.com>
2020-12-08 20:33:44 +0530

    Add files via upload

---
+++ Block.js
@@ -0,0 +1,29 @@
+class Block{
+    constructor(x, y, width, height) {
+        var options = {
+           
+            restitution :0.4,
+            friction :0.0,
+            //isStatic:true
+           
+        }
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        this.width = width;
+        this.height = height;
+        this.Visibility = 255
+        World.add(world, this.body);
+      }
+      display(){
+        if(this.body.speed<8){
+         //body.display();
+        }
+        else{
+          World.remove(world,this.body);
+          push ();
+          this.Visibility = this.Visibility - 5
+          tint(255,this.Visibility);
+         // image(this.image,this.body.position.x,this.body.position.y,50,50);
+          pop ();
+        }
+       }
+     }
